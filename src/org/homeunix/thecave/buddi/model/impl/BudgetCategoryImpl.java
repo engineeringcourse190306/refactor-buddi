1572f088f6700904fc6e578113edae1b47ed9bce
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2019-03-06 17:14:33 +0800

    :recycle: remove period key

--- src/org/homeunix/thecave/buddi/model/impl/BudgetCategoryImpl.java
+++ src/org/homeunix/thecave/buddi/model/impl/BudgetCategoryImpl.java
@@ -115,10 +115,9 @@ public class BudgetCategoryImpl extends SourceImpl implements BudgetCategory {
         double totalStartPeriod = getAmountInPeriod(new Period(period.getStartDate(), beginBudgetPeriod.getEndDate()));
 
         double totalInMiddle = 0;
-        for (String periodKey : getBudgetPeriods(
-                beginBudgetPeriod.nextBudgetPeriod().getStartDate(),
-                endBudgetPeriod.previousBudgetPeriod().getStartDate())) {
-            totalInMiddle += getAmountOfBudgetPeriod(getPeriodDate(periodKey));
+        List<BudgetPeriod> budgetPeriods = getBudgetPeriods(beginBudgetPeriod.nextBudgetPeriod(), endBudgetPeriod.previousBudgetPeriod());
+        for (BudgetPeriod budgetPeriod : budgetPeriods) {
+            totalInMiddle += getAmountOfBudgetPeriod(budgetPeriod.getStartDate());
         }
 
         double totalEndPeriod = getAmountInPeriod(new Period(endBudgetPeriod.getStartDate(), period.getEndDate()));
@@ -144,21 +143,21 @@ public class BudgetCategoryImpl extends SourceImpl implements BudgetCategory {
      * Returns a list of BudgetPeriods, covering the entire range of periods
      * occupied by startDate to endDate.
      *
-     * @param startDate
-     * @param endDate
+     * @param beginBudgetPeriod
+     * @param endBudgetPeriod
      * @return
      */
-    public List<String> getBudgetPeriods(Date startDate, Date endDate) {
-        List<String> budgetPeriodKeys = new LinkedList<String>();
+    public List<BudgetPeriod> getBudgetPeriods(BudgetPeriod beginBudgetPeriod, BudgetPeriod endBudgetPeriod) {
+        List<BudgetPeriod> budgetPeriods = new ArrayList<>();
 
-        Date temp = getBudgetPeriodType().getStartOfBudgetPeriod(startDate);
+        BudgetPeriod current = beginBudgetPeriod;
 
-        while (temp.before(getBudgetPeriodType().getEndOfBudgetPeriod(endDate))) {
-            budgetPeriodKeys.add(getPeriodKey(temp));
-            temp = getBudgetPeriodType().getBudgetPeriodOffset(temp, 1);
+        while (current.getStartDate().before(endBudgetPeriod.getEndDate())) {
+            budgetPeriods.add(current);
+            current = current.nextBudgetPeriod();
         }
 
-        return budgetPeriodKeys;
+        return budgetPeriods;
     }
 
     /**
